58469c5e9506a31e2cc3d07f41db6774ab10082f
Andrea Maria Regalli <a.regalli@reply.it>
2025-05-22 18:09:07 +0200

    introdotto filtro ricerca fermi per tipo

--- src/main/webapp/app/entity/shutdown/list/shutdown.filter.ts
+++ src/main/webapp/app/entity/shutdown/list/shutdown.filter.ts
@@ -7,6 +7,7 @@ import { IParam, ISortField, TypeData } from '../../../shared/pagination/filter.
 })
 export class ShutdownFilter extends IFilterPagination {
   static PARTNER: IParam = { name: 'partner', req: 'partnerId', type: TypeData.PARTNER, defaultValue: '' };
+  static TYPE: IParam = { name: 'type', req: 'typePlanned', type: TypeData.STRING, defaultValue: '' };
 
   sort: ISortField = { field: 'shutdownStartDate', direction: 'desc' };
   sortDefault: ISortField = { field: 'shutdownStartDate', direction: 'desc' };
