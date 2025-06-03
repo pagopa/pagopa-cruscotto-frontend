db80e85f9a9953dbaa4fa3421684516a553cfd85
D00B210 <matteo.tofoni@nexigroup.com>
2025-06-03 14:42:33 +0200

    job details

--- src/main/webapp/app/admin/job/job.service.ts
+++ src/main/webapp/app/admin/job/job.service.ts
@@ -3,7 +3,7 @@ import { HttpClient, HttpResponse } from '@angular/common/http';
 import { map, Observable } from 'rxjs';
 
 import { ApplicationConfigService } from '../../core/config/application-config.service';
-import { IJob } from './job.model';
+import { IJob, IJobExecution } from './job.model';
 import { createRequestOption } from '../../core/request/request-util';
 import dayjs from 'dayjs/esm';
 import { DATE_TIME_FORMAT_ISO } from '../../config/input.constants';
@@ -16,8 +16,17 @@ type JobRestOf<T extends IJob> = Omit<T, 'scheduleTime' | 'lastFiredTime' | 'nex
 
 export type RestJob = JobRestOf<IJob>;
 
+type ExecutionRestOf<T extends IJobExecution> = Omit<T, 'scheduledTime' | 'initFiredTime' | 'endFiredTime'> & {
+  scheduledTime?: string | null;
+  initFiredTime?: string | null;
+  endFiredTime?: string | null;
+};
+
+export type RestExecution = ExecutionRestOf<IJobExecution>;
+
 type EntityResulCodeResponseType = HttpResponse<number>;
 type EntityArrayResponseType = HttpResponse<IJob[]>;
+type ExecutionArrayResponseType = HttpResponse<IJobExecution[]>;
 
 @Injectable({ providedIn: 'root' })
 export class JobService {
@@ -57,6 +66,14 @@ export class JobService {
     return this.http.get<number>(`${this.resourceUrl}/update`, { params: options, observe: 'response' });
   }
 
+  execution(req?: any): Observable<ExecutionArrayResponseType> {
+    const options = createRequestOption(req);
+
+    return this.http
+      .get<RestExecution[]>(`${this.resourceUrl}/executions`, { params: options, observe: 'response' })
+      .pipe(map(res => this.convertExecutionResponseArrayFromServer(res)));
+  }
+
   protected convertJobResponseArrayFromServer(res: HttpResponse<RestJob[]>): HttpResponse<IJob[]> {
     return res.clone({
       body: res.body ? res.body.map(item => this.convertJobFromServer(item)) : null,
@@ -69,6 +86,21 @@ export class JobService {
       scheduleTime: restJob.scheduleTime ? dayjs(restJob.scheduleTime, DATE_TIME_FORMAT_ISO) : undefined,
       lastFiredTime: restJob.lastFiredTime ? dayjs(restJob.lastFiredTime, DATE_TIME_FORMAT_ISO) : undefined,
       nextFireTime: restJob.nextFireTime ? dayjs(restJob.nextFireTime, DATE_TIME_FORMAT_ISO) : undefined,
+    };
+  }
+
+  protected convertExecutionResponseArrayFromServer(res: HttpResponse<RestExecution[]>): HttpResponse<IJobExecution[]> {
+    return res.clone({
+      body: res.body ? res.body.map(item => this.convertExecutionFromServer(item)) : null,
+    });
+  }
+
+  protected convertExecutionFromServer(restExecution: RestExecution): IJobExecution {
+    return {
+      ...restExecution,
+      scheduledTime: restExecution.scheduledTime ? dayjs(restExecution.scheduledTime, DATE_TIME_FORMAT_ISO) : undefined,
+      initFiredTime: restExecution.initFiredTime ? dayjs(restExecution.initFiredTime, DATE_TIME_FORMAT_ISO) : undefined,
+      endFiredTime: restExecution.endFiredTime ? dayjs(restExecution.endFiredTime, DATE_TIME_FORMAT_ISO) : undefined,
     };
   }
 }
