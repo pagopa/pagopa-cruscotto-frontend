91e0e74188a7e954d81b665dda09f24fed2f6185
Andrea Maria Regalli <a.regalli@reply.it>
2025-06-09 12:28:05 +0200

    modifiche per tipo analisi manuale in update e create

--- src/main/webapp/app/entity/module/update/module-form.service.ts
+++ src/main/webapp/app/entity/module/update/module-form.service.ts
@@ -122,6 +122,7 @@ export class ModuleFormService {
         ...moduleRawValue,
         id: { value: moduleRawValue.id, disabled: true },
         code: { value: moduleRawValue.code, disabled: true },
+        analysisType: { value: 'MANUALE', disabled: true },
       } as any /* cast to workaround https://github.com/angular/angular/issues/46458 */,
     );
   }
@@ -133,7 +134,7 @@ export class ModuleFormService {
     form.reset(
       {
         ...moduleRawValue,
-        analysisType: { value: '', disabled: false },
+        analysisType: { value: 'MANUALE', disabled: true },
         allowManualOutcome: { value: '', disabled: false },
         status: { value: '', disabled: false },
         configAverageTimeLimit: { value: '', disabled: false },
