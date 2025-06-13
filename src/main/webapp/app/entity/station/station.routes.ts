7875c0c50a7675ee2066680d2b08cd263af2df0e
Fagiani Sergio <sergio.fagiani@nexigroup.com>
2025-06-13 10:39:47 +0200

    Creazione e assegnazione delle funzioni alle nuove funzionalità

--- src/main/webapp/app/entity/station/station.routes.ts
+++ src/main/webapp/app/entity/station/station.routes.ts
@@ -3,16 +3,23 @@ import { UserRouteAccessService } from 'app/core/auth/user-route-access.service'
 import { StationComponent } from './list/station.component';
 import StationDetailComponent from './detail/station-detail.component';
 import { StationRoutingResolve } from './route/station-routing-resolve.service';
+import { Authority } from 'app/config/authority.constants';
 
 const stationRoutes: Routes = [
   {
     path: '',
     component: StationComponent,
+    data: {
+      authorities: [Authority.STATION_INQUIRY],
+    },
     canActivate: [UserRouteAccessService],
   },
   {
     path: ':id/view',
     component: StationDetailComponent,
+    data: {
+      authorities: [Authority.STATION_INQUIRY],
+    },
     resolve: {
       station: StationRoutingResolve,
     },
