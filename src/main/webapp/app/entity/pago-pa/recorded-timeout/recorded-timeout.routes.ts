bc558762da8cd857638557771cfbf9872ba3cdb1
Fagiani Sergio <sergio.fagiani@nexigroup.com>
2025-06-13 10:39:47 +0200

    Creazione e assegnazione delle funzioni alle nuove funzionalità

--- src/main/webapp/app/entity/pago-pa/recorded-timeout/recorded-timeout.routes.ts
+++ src/main/webapp/app/entity/pago-pa/recorded-timeout/recorded-timeout.routes.ts
@@ -1,11 +1,15 @@
 import { Routes } from '@angular/router';
 import { UserRouteAccessService } from 'app/core/auth/user-route-access.service';
 import { PagoPaRecordedTimeoutComponent } from './list/recorded-timeout.component';
+import { Authority } from 'app/config/authority.constants';
 
 const recordedTimeoutRoutes: Routes = [
   {
     path: '',
     component: PagoPaRecordedTimeoutComponent,
+    data: {
+      authorities: [Authority.PAGOPA_RECORDED_TIMEOUT_INQUIRY],
+    },
     canActivate: [UserRouteAccessService],
   },
 ];
--- src/main/webapp/app/entity/partner/partner.routes.ts
+++ src/main/webapp/app/entity/partner/partner.routes.ts
@@ -3,16 +3,23 @@ import { UserRouteAccessService } from 'app/core/auth/user-route-access.service'
 import { PartnerComponent } from './list/partner.component';
 import PartnerDetailComponent from './detail/partner-detail.component';
 import { PartnerRoutingResolve } from './route/partner-routing-resolve.service';
+import { Authority } from 'app/config/authority.constants';
 
 const partnerRoutes: Routes = [
   {
     path: '',
     component: PartnerComponent,
+    data: {
+      authorities: [Authority.PARTNER_INQUIRY],
+    },
     canActivate: [UserRouteAccessService],
   },
   {
     path: ':id/view',
     component: PartnerDetailComponent,
+    data: {
+      authorities: [Authority.PARTNER_INQUIRY],
+    },
     resolve: {
       partner: PartnerRoutingResolve,
     },
