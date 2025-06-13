768f300211f3f98a4cbba708fc61e9a5e9c2f96b
Andrea Maria Regalli <a.regalli@reply.it>
2025-06-13 12:16:26 +0200

    introdotti filtri partner e stazione in pagine pagoPa

--- src/main/webapp/app/entity/pago-pa/payment-receipt/list/payment-receipt.filter.ts
+++ src/main/webapp/app/entity/pago-pa/payment-receipt/list/payment-receipt.filter.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@angular/core';
-import { ISortField } from 'app/shared/pagination/filter.model';
+import { IParam, ISortField, TypeData } from 'app/shared/pagination/filter.model';
 import { IFilterPagination } from 'app/shared/pagination/filter.pagination';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PaymentReceiptFilter extends IFilterPagination {
+  static PARTNER: IParam = { name: 'cfPartner', req: 'cfPartner', type: TypeData.STRING, defaultValue: '' };
+  static STATION: IParam = { name: 'station', req: 'station', type: TypeData.STRING, defaultValue: '' };
   sort: ISortField = { field: 'startDate', direction: 'asc' };
   sortDefault: ISortField = { field: 'startDate', direction: 'asc' };
 }
