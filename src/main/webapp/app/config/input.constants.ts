712e9f6765ad7576694da3694478078a5925cdf9
leonardo.cenzi <leonardo.cenzi@external.nexigroup.com>
2025-07-30 17:19:26 +0200

    fix date formats
    the new TypeData will use the DATE_FORMAT format, rollback of the constants file

--- src/main/webapp/app/config/input.constants.ts
+++ src/main/webapp/app/config/input.constants.ts
@@ -1,5 +1,5 @@
-export const DATE_FORMAT = 'DD/MM/YYYY';
+export const DATE_FORMAT = 'YYYY-MM-DD';
 export const DATE_TIME_FORMAT = 'DD-MM-YYYY HH:mm:ss';
-export const DATE_FORMAT_ISO = 'YYYY-MM-DD';
+export const DATE_FORMAT_ISO = 'DD/MM/YYYY';
 export const DATE_TIME_FORMAT_ISO = 'YYYY-MM-DDTHH:mm:ssZ';
 export const HOME_PAGE_DAY_VIEW_ALERT_MESSAGE = 15;
--- src/main/webapp/app/entity/station/list/station.filter.ts
+++ src/main/webapp/app/entity/station/list/station.filter.ts
@@ -7,7 +7,7 @@ import { IParam, ISortField, TypeData } from '../../../shared/pagination/filter.
 })
 export class StationFilter extends IFilterPagination {
   static PARTNER: IParam = { name: 'partner', req: 'partnerId', type: TypeData.PARTNER, defaultValue: '' };
-  static STATION: IParam = { name: 'station', req: 'station', type: TypeData.STATION, defaultValue: '' };
+  static STATION: IParam = { name: 'station', req: 'stationId', type: TypeData.STATION, defaultValue: '' };
   static SHOW_NOT_ACTIVE: IParam = { name: 'showNotActive', req: 'showNotActive', type: TypeData.BOOLEAN, defaultValue: null };
   sort: ISortField = { field: 'id', direction: 'desc' };
   sortDefault: ISortField = { field: 'id', direction: 'desc' };
