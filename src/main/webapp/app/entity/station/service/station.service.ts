6ab2e3edf972a40c8670b6e70d76ec4a10b2db87
leonardo.cenzi <leonardo.cenzi@external.nexigroup.com>
2025-08-07 16:06:38 +0200

    CRUS-159

--- src/main/webapp/app/entity/station/service/station.service.ts
+++ src/main/webapp/app/entity/station/service/station.service.ts
@@ -28,6 +28,13 @@ export class StationService {
   query(req?: any): Observable<EntityArrayResponseType> {
     const options = createRequestOption(req);
     return this.http
+      .get<RestStation[]>(this.resourceDetailUrl, { params: options, observe: 'response' })
+      .pipe(map(res => this.convertStationResponseArrayFromServer(res)));
+  }
+
+  queryLookup(req?: any): Observable<EntityArrayResponseType> {
+    const options = createRequestOption(req);
+    return this.http
       .get<RestStation[]>(this.resourceUrl, { params: options, observe: 'response' })
       .pipe(map(res => this.convertStationResponseArrayFromServer(res)));
   }
