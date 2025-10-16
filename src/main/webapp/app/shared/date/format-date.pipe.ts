e32ad58776f23b7bdad7bf8b7a0cdf8b53c9a6c3
Dario Marco Bellini <dario.bellini@external.nexigroup.com>
2025-10-16 17:08:37 +0200

    KPI B.4: Fix dataDate render error + dataDate sorting from older to newer

--- src/main/webapp/app/shared/date/format-date.pipe.ts
+++ src/main/webapp/app/shared/date/format-date.pipe.ts
@@ -1,12 +1,14 @@
 import { Pipe, PipeTransform } from '@angular/core';
-
 import dayjs from 'dayjs/esm';
-/* eslint-disable no-console */
+
 @Pipe({
   name: 'formatDate',
 })
 export default class FormatDatePipe implements PipeTransform {
-  transform(day: dayjs.Dayjs | null | undefined, format: string, locale: string): string {
-    return day ? day.locale(locale).format(format) : '';
+  transform(date: dayjs.Dayjs | Date | null | undefined, format: string, locale: string): string {
+    if (!date) return '';
+
+    const d = dayjs.isDayjs(date) ? date : dayjs(date);
+    return d.isValid() ? d.locale(locale).format(format) : '';
   }
 }
