15c84c4a98544f30eb95805ab44548e6a75ae684
leonardo.cenzi <leonardo.cenzi@external.nexigroup.com>
2026-03-09 15:10:16 +0100

    changed redirectUri for sso

--- src/main/webapp/environments/environment.ts
+++ src/main/webapp/environments/environment.ts
@@ -4,8 +4,8 @@ export const environment = {
     auth: {
       clientId: '10eb962e-531c-4f0c-b216-bb0539bba43b',
       authority: 'https://login.microsoftonline.com/7788edaf-0346-4068-9d79-c868aed15b3d',
-      redirectUri: 'http://localhost:4200',
-      postLogoutRedirectUri: 'http://localhost:4200',
+      redirectUri: window.location.origin,
+      postLogoutRedirectUri: window.location.origin,
     },
     apiScopes: ['api://pagopa-d-crusc8-ui/user_impersonation'],
   },
--- src/main/webapp/environments/environment.uat.ts
+++ src/main/webapp/environments/environment.uat.ts
@@ -4,8 +4,8 @@ export const environment = {
     auth: {
       clientId: 'TODO_UAT_CLIENT_ID',
       authority: 'https://login.microsoftonline.com/TODO_UAT_TENANT_ID',
-      redirectUri: 'https://crusc8.uat.platform.pagopa.it',
-      postLogoutRedirectUri: 'https://crusc8.uat.platform.pagopa.it',
+      redirectUri: window.location.origin,
+      postLogoutRedirectUri: window.location.origin,
     },
     apiScopes: ['api://pagopa-u-crusc8-ui/user_impersonation'],
   },
