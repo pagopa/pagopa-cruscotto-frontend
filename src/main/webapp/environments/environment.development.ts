50425e05b10b41082886de2de664830461d78ea7
leonardo.cenzi <leonardo.cenzi@external.nexigroup.com>
2026-03-09 12:59:01 +0100

    fix to release and evironment files

--- src/main/webapp/environments/environment.development.ts
+++ src/main/webapp/environments/environment.development.ts
@@ -4,8 +4,8 @@ export const environment = {
     auth: {
       clientId: '10eb962e-531c-4f0c-b216-bb0539bba43b',
       authority: 'https://login.microsoftonline.com/7788edaf-0346-4068-9d79-c868aed15b3d',
-      redirectUri: 'https://crusc8.dev.platform.pagopa.it',
-      postLogoutRedirectUri: 'https://crusc8.dev.platform.pagopa.it',
+      redirectUri: 'http://localhost:9000',
+      postLogoutRedirectUri: 'http://localhost:9000',
     },
     apiScopes: ['api://pagopa-d-crusc8-ui/user_impersonation'],
   },
