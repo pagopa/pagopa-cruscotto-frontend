5f04305d1cef0404c9764b5e9dd52bf02c1e5e48
leonardo.cenzi <leonardo.cenzi@external.nexigroup.com>
2026-03-09 16:11:54 +0100

    rollback env files

--- src/main/webapp/environments/environment.prod.ts
+++ src/main/webapp/environments/environment.prod.ts
@@ -4,8 +4,8 @@ export const environment = {
     auth: {
       clientId: 'TODO_PROD_CLIENT_ID',
       authority: 'https://login.microsoftonline.com/TODO_PROD_TENANT_ID',
-      redirectUri: window.location.origin,
-      postLogoutRedirectUri: window.location.origin,
+      redirectUri: 'https://crusc8.platform.pagopa.it',
+      postLogoutRedirectUri: 'https://crusc8.platform.pagopa.it',
     },
     apiScopes: ['api://pagopa-p-crusc8-ui/user_impersonation'],
   },
