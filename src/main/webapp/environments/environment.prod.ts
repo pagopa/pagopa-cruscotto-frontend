16592e71dabb7341b36ba9b946a43de60993ef2b
leonardo.cenzi <leonardo.cenzi@external.nexigroup.com>
2026-03-18 18:43:13 +0100

    set environment variables

--- src/main/webapp/environments/environment.prod.ts
+++ src/main/webapp/environments/environment.prod.ts
@@ -2,9 +2,9 @@ export const environment = {
   DEBUG_INFO_ENABLED: false,
   msalConfig: {
     auth: {
-      clientId: 'TODO_PROD_CLIENT_ID',
-      authority: 'https://login.microsoftonline.com/TODO_PROD_TENANT_ID',
-      redirectUri: 'https://crusc8.platform.pagopa.it',
+      clientId: '5a3a6ad5-b4bc-49aa-a111-23279a0256ed',
+      authority: 'https://login.microsoftonline.com/7788edaf-0346-4068-9d79-c868aed15b3d',
+      redirectUri: 'https://crusc8.platform.pagopa.it/',
       postLogoutRedirectUri: 'https://crusc8.platform.pagopa.it',
     },
     apiScopes: ['api://pagopa-p-crusc8-ui/user_impersonation'],
